eb9a1c5b1a94f156c118b995257f8d4af59b0f63
Mateus Schmitz <matteuschmitz@gmail.com>
2014-05-20 20:54:11 -0300

    init repo

---
+++ src/module/site/controller/BaseController.php
@@ -0,0 +1,9 @@
+<?php
+
+class BaseController
+{
+	function __construct()
+	{
+		
+	}
+}
---
+++ src/module/site/controller/IndexController.php
@@ -0,0 +1,9 @@
+<?php
+
+class IndexController extends BaseController
+{
+	function __construct()
+	{
+		
+	}
+}
