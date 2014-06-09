7e28281e2bea3f9ac18ecc8bdffb7412ce8c33ba
Mateus Schmitz <mateus.schmitz@kinghost.com.br>
2014-06-09 18:37:48 -0300

    Instalado Slim

--- public/index.php
+++ public/index.php
@@ -1,9 +1,10 @@
 <?php
 
-require('../bootstrap.php');
+// require('../bootstrap.php');
 
-chdir(dirname(__DIR__));
+// chdir(dirname(__DIR__));
 
-//echo "<pre>" . print_r($_GET, 1);
+// //echo "<pre>" . print_r($_GET, 1);
 
-echo "<pre>" . print_r(runApp($_GET), 1);
+// echo "<pre>" . print_r(runApp($_GET), 1);
+require '../vendor/autoload.php';
